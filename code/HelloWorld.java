eb6c89a6e7a8a3bbce49e2b4672480d5c8cdb35a
fujiaxu <858813020@qq.com>
2020-11-08 18:42:44 +0800

    代码目录

---
+++ code/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*类的注释信息
+*@version:1.0
+*author fujiaxu
+*/
+
+public class HelloWorld{//这是一个类叫做HelloWorld
+
+	public static void main(String [] args){
+
+			System.out.println("HelloWorld");
+	}
+
+}
